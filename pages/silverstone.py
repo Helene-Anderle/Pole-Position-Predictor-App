c6b3790ebfa5bf0c692226efcc77baf585864f6d
unknown <helene.anderle@gmx.at>
2026-04-26 20:28:15 +0100

    Silverstone page fixer upper

--- pages/silverstone.py
+++ pages/silverstone.py
@@ -1,22 +1,28 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
-st.title("Silverstone Circuit")
+st.title("Silverstone Analysis")
 
-st.image("https://upload.wikimedia.org/wikipedia/commons/3/3c/Silverstone_Circuit_map.png")
+# ---------------- IMAGE ----------------
+st.image("pages/silverstone.png", use_container_width=True)
 
+st.divider()
+
+# ---------------- DATA ----------------
 data = pd.DataFrame({
     "Year": [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
     "PoleTime": [92.937, 89.287, 86.600, 85.892, 85.093, 85.000, 86.760, 100.983, 86.720, 85.819]
 })
 
-st.dataframe(data)
+st.subheader("Pole Position Trend")
 
-model = LinearRegression()
-model.fit(data[["Year"]], data["PoleTime"])
+# ---------------- GRAPH ----------------
+fig, ax = plt.subplots()
+ax.plot(data["Year"], data["PoleTime"], marker="o")
 
-prediction = model.predict(np.array([[2025]]))
+ax.set_xlabel("Year")
+ax.set_ylabel("Pole Time (s)")
+ax.set_title("Silverstone Pole Position Trend")
 
-st.metric("2025 Prediction", f"{prediction[0]:.3f} s")
+st.pyplot(fig)
