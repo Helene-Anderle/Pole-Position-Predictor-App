b581653b128c1de6691eba14c71ecb671fb4fb67
unknown <helene.anderle@gmx.at>
2026-04-26 20:32:52 +0100

    Silverstone error finding

--- pages/silverstone.py
+++ pages/silverstone.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import streamlit as st
+
+st.error("SILVERSTONE PAGE IS RUNNING")
+
 st.title("Silverstone Analysis")
 
 # ---------------- IMAGE ----------------
