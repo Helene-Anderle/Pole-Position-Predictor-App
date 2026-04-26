80ca6119c8a47bdc7af2f283fd1238ea21e91422
unknown <helene.anderle@gmx.at>
2026-04-26 20:10:40 +0100

    bug fix2

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.image("https://upload.wikimedia.org/wikipedia/commons/3/3c/Silverstone_Circuit_map.png")
     if st.button("Silverstone"):
-        st.switch_page("Silverstone.py")
+        st.switch_page("Silverstone")
 
 with col2:
     st.image("https://upload.wikimedia.org/wikipedia/commons/9/9e/Monaco_circuit.png")
